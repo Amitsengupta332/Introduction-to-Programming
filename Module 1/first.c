4258d0d3f5e255a7a4efa96b6f5d0f60d5a2ac65
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-06 01:20:51 +0600

    first commit

---
+++ Module 1/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num1 =5;
+    num1 = 10;
+    float f = 2.5;
+    char c = '9';
+    printf("%d %.2f %c", num1,f,c);
+    // printf("hello \n");
+    // printf("world");
+    return 0;
+}
