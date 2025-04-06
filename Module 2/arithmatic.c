b775e40fcc15534e35f3b7636887748febdd99f3
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-06 22:02:58 +0600

    arithmatic

---
+++ Module 2/arithmatic.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int a = 10;
+    int b=5;
+    // int sum= a + b;
+    // printf("Summation = %d\n", sum);
+    // int sub = a - b;
+    // printf("Subtraction = %d\n", sub);
+    // int mul = a * b;
+    // printf("Multiplication = %d\n", mul);
+    int div = a / b;
+    printf("Division = %d", div);
+    
+    return 0;
+}
