4a1ca7a9d68ec05d588ec4beeed83ae8011fd31f
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-07 01:48:03 +0600

    done quiz solve

--- Module 2/arithmatic.c
+++ Module 2/arithmatic.c
@@ -1,16 +1,49 @@
 #include<stdio.h>
 
 int main(){
-    int a = 10;
-    int b=5;
-    // int sum= a + b;
-    // printf("Summation = %d\n", sum);
-    // int sub = a - b;
-    // printf("Subtraction = %d\n", sub);
-    // int mul = a * b;
-    // printf("Multiplication = %d\n", mul);
-    int div = a / b;
-    printf("Division = %d", div);
+    int x =5;
+    // int y = 12;
+    if( x<10){
+        printf("hello ");
+    }
+    if (x == 5){
+        printf("hi");
+    }
+    else {
+        printf ("no");
+    }
+
+
+        // printf("hello\n");
+
+    // int num = 6;
+    // if(num>0){
+    //     // printf("hi");
+    //     if (num<10)
+    //     {
+    //         printf("small positive number\n");
+    //         /* code */
+    //     }
+        
+
+    // }
+
+
+  
+    // // int a = 15;
+    // double a = 15/4;
+    // printf("%lf", a);
+    // int b=4;
+    // // int sum= a + b;
+    // // printf("Summation = %d\n", sum);
+    // // int sub = a - b;
+    // // printf("Subtraction = %d\n", sub);
+    // // int mul = a * b;
+    // // printf("Multiplication = %d\n", mul);
+    // // int div = a / b;
+    // // printf("Division = %d", div);
+    // // int mod = a % b;
+    // // printf(" Modulus = %d",a);
     
     return 0;
 }
