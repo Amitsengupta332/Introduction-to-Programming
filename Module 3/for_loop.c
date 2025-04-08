0e4fb3ada6f1f697ac08a926830e641f2b04fed1
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-09 01:41:14 +0600

    done sum of n number

--- Module 3/for_loop.c
+++ Module 3/for_loop.c
@@ -3,10 +3,27 @@
 int main()
 
 {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-    for (int i = 1; i <= 10; i++)
+    for (int i = 1; i <= n; i++)
     {
-        printf("I am sorry \n"); 
+        // printf("I am sorry \n"); 
+        printf("%d\n", i);
     }
     return 0;
 }
+
+// #include<stdio.h>
+
+// int main() 
+// {
+//     int n;
+//     scanf("%d", &n);
+//     for(int i =1; i<=n ; i++)
+//     {
+//         printf("%d\n", i);
+//     }
+//     return 0;
+// }
---
+++ Module 3/loop_with_condition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int n = 14;
+    if (n%2 == 0)
+    {
+        printf("Even number\n");
+        /* code */
+    }
+    else 
+    {
+        printf("Odd number\n");
+        /* code */
+    }
+    
+    return 0;
+}
