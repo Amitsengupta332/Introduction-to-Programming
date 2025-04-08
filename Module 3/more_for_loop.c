3872933a58204664897f2abe48ee407d16b0e8c9
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-09 00:43:02 +0600

    done odd even multiplication

--- Module 3/more_for_loop.c
+++ Module 3/more_for_loop.c
@@ -2,7 +2,19 @@
 
 int main()
 {
-    for (int i = 1; i <= 10; i = i+2)
+    //odd
+    // for (int i = 1; i <= 10; i = i+2)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    //even
+    for (int i = 2; i <= 10; i = i+2)
+    {
+        // printf("%d\n", i);
+    }
+    //multiple of 4
+    for (int i = 4; i <= 100; i = i+4)
     {
         printf("%d\n", i);
     }
