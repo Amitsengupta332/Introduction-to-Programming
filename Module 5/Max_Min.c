df93efa07b8c2bfbce506e43af7523cd5c9f1023
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-13 02:16:56 +0600

    min max

--- Module 5/Max_Min.c
+++ Module 5/Max_Min.c
@@ -4,20 +4,37 @@ int main()
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
 
-    if(a > b && a > c){
+    // minimum
+    if (a <= b && a <= c)
+    {
+        printf("%d ", a);
+    }
+    else if (b <= a && b <= c)
+    {
+        printf("%d ", b);
+        /* code */
+    }
+    else if (c <= a && c <= b)
+    {
+        printf("%d ", c);
+        /* code */
+    }
+
+    // maximum
+    if (a >= b && a >= c)
+    {
         printf("%d", a);
-    }else if (b>a && b>c)
+    }
+    else if (b >= a && b >= c)
     {
         printf("%d", b);
         /* code */
     }
-    else if (c>a && c> b)
+    else if (c >= a && c >= b)
     {
         printf("%d", c);
         /* code */
     }
-    
-    
 
     return 0;
 }
