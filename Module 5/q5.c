cd9dd74086c76ddac3125b191429b582439b94d6
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-13 02:31:31 +0600

    5

---
+++ Module 5/q5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int marks = 76;
+
+    if (marks >= 80)
+    {
+        printf("A+ Grade\n");
+    }
+    else if (marks >= 70)
+    {
+        printf("A Grade\n");
+    }
+    else if (marks >= 70)
+    {
+        printf("B Grade\n");
+    }
+
+    return 0;
+}
