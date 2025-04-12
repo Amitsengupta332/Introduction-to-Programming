5d6ba381ef9945d59e6af6ad3ec20bb1019d7b0b
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-13 02:28:54 +0600

    4

---
+++ Module 5/q4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int x = 0;
+    if( x== 1){
+        if(x == 0){
+            printf("hello\n");
+        }else{
+            printf("hi\n");
+        }
+    }
+    else{
+        printf("no\n");
+    }
+    return 0;
+}
