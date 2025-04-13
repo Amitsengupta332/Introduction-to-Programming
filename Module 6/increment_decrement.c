322d5c338eb7c289144bcaee4da5390b101489a9
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-13 21:42:13 +0600

    increment

---
+++ Module 6/increment_decrement.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int x = 10;
+    // int y = x++;
+    int y = ++x;
+    printf("%d %d", x, y);
+    // for (int i = 1; i <= 5; i++)
+    // {
+    //     printf("%d\n", i);
+    //     /* code */
+    // }
+    
+    return 0;
+}
