802fa1a4acc75b3f565a58c8592444307aadcd56
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-14 16:51:42 +0600

    done q1

---
+++ Module 6/quiz1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("hi ");
+        if(i>2){
+            break;
+        }
+        /* code */
+    }
+    
+    return 0;
+}
