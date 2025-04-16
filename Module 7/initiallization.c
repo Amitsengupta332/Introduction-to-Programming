0859ab7259372bb55dc6002ea607a61d6fdf65d0
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-16 21:26:47 +0600

    done initialization

---
+++ Module 7/initiallization.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x = 10; // declare and initialize
+    int a[] = {1, 2, 3, 4, 5}; // declare and initialize
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", a[i]); // print the array
+        /* code */
+    }
+    
+    return 0;
+}
