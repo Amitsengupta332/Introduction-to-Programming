b8ace2bc52fb6c844281eb117ba54a8961c65120
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-16 20:49:05 +0600

    done user input and output array

--- Module 7/array.c
+++ Module 7/array.c
@@ -2,9 +2,25 @@
 int main()
 {
     int arr[5];
-    arr[2] = 10;
-    arr[3] = 20;
-    arr[1] = 50;
-    printf("%d\n", arr[2]);
+    // scanf("%d %d %d %d %d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
+
+    // printf("%d %d %d %d %d\n", arr[0], arr[1], arr[2], arr[3], arr[4]);
+
+    // array print with the help of loop
+
+    for(int i = 0; i <=4; i++)
+    {
+        scanf("%d", &arr[i]);
+        // printf("a[%d]\n", i);
+    } 
+    for(int i = 0; i <=4; i++)
+    {
+        // scanf("%d", &arr[i]);
+        printf("%d ", arr[i]);
+    }
+    // arr[2] = 10;
+    // arr[3] = 20;
+    // arr[1] = 50;
+    // printf("%d\n", arr[2]);
     return 0;
 }
