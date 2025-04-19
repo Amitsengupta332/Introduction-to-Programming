b8e8568fbbe8fa44ad46de53180d4596a844d87f
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-20 00:34:18 +0600

    done insert

---
+++ Module 9/insert.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n+1];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        /* code */
+    }
+    int idx, val;
+    scanf("%d %d", &idx, &val);
+    for(int i = n; i >= idx+1; i--)
+    {
+        a[i] = a[i-1];
+    }
+
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d ", a[i]);
+        /* code */
+    }
+    
+    
+    return 0;
+}
