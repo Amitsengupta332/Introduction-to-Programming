010bba0d578bbf1c8baedbb090f20c9ac5a640c4
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-21 11:55:08 +0600

    function string lenght

--- Module 10/length_of_string.c
+++ Module 10/length_of_string.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
     char s[101];
     scanf("%s", s);
-    int count = 0;
-    for (int i = 0; s[i] != '\0'; i++)
-    {
-        count++;
-        /* code */
-    }
-    printf("%d", count);
+    // int count = 0;
+    // for (int i = 0; s[i] != '\0'; i++)
+    // {
+    //     count++;
+    //     /* code */
+    // }
+    // printf("%d", count);
+
+    int sz = strlen(s);
+    printf("%d", sz);
 
     return 0;
 }
