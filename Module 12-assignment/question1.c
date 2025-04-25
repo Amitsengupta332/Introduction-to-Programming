86b3ba043c86a1fcd4322933339e020d68a44df5
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-25 23:33:34 +0600

    solve contest 1

---
+++ Module 12-assignment/question1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        /* code */
+    }
+
+    int cnt1 = 0, cnt2 = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] % 2 == 0)
+        {
+            cnt1++;
+        }
+        else if (a[i] % 3 == 0)
+        {
+            cnt2++;
+            /* code */
+        }
+
+        /* code */
+    }
+    printf("%d %d", cnt1, cnt2);
+
+    return 0;
+}
