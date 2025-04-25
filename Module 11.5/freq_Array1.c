96f5dc49b32afcf477a60c13c81390f187cb489b
Amitsengupta332 <amitsengupta332@gmail.com>
2025-04-25 21:57:40 +0600

    done

--- Module 11.5/freq_Array1.c
+++ Module 11.5/freq_Array1.c
@@ -13,42 +13,47 @@ int main()
     int freq[6] = {0};
     for (int i = 0; i < n; i++)
     {
-        if (a[i] == 0)
-        {
-            // cnt0++;
-            freq[0]++;
-            /* code */
-        }
-        else if (a[i] == 1)
-        {
-            // cnt1++;
-            freq[1]++;
-            /* code */
-        }
-        else if (a[i] == 2)
-        {
-            // cnt2++;
-            freq[2]++;
-            /* code */
-        }
-        else if (a[i] == 3)
-        {
-            // cnt3++;
-            freq[3]++;
-            /* code */
-        }
-        else if (a[i] == 4)
-        {
-            // cnt4++;
-            freq[4]++;
-            /* code */
-        }
-        else if (a[i] == 5)
-        {
-            // cnt5++;
-            freq[5]++;
-            /* code */
-        }
+        int val = a[i];
+        // freq[a[i]]++;
+        freq[val]++;
+
+        
+        // if (a[i] == 0)
+        // {
+        //     // cnt0++;
+        //     freq[0]++;
+        //     /* code */
+        // }
+        // else if (a[i] == 1)
+        // {
+        //     // cnt1++;
+        //     freq[1]++;
+        //     /* code */
+        // }
+        // else if (a[i] == 2)
+        // {
+        //     // cnt2++;
+        //     freq[2]++;
+        //     /* code */
+        // }
+        // else if (a[i] == 3)
+        // {
+        //     // cnt3++;
+        //     freq[3]++;
+        //     /* code */
+        // }
+        // else if (a[i] == 4)
+        // {
+        //     // cnt4++;
+        //     freq[4]++;
+        //     /* code */
+        // }
+        // else if (a[i] == 5)
+        // {
+        //     // cnt5++;
+        //     freq[5]++;
+        //     /* code */
+        // }
 
         /* code */
     }
@@ -58,12 +63,19 @@ int main()
     // printf("%d\n", cnt4);
     // printf("%d\n", cnt5);
 
-    printf("%d -> %d\n",1, freq[0]);
-    printf("%d\n", freq[1]);
-    printf("%d\n", freq[2]);
-    printf("%d\n", freq[3]);
-    printf("%d\n", freq[4]);
-    printf("%d\n", freq[5]);
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d -> %d\n",1, freq[i]);
+        /* code */
+    }
+    
+
+    // printf("%d -> %d\n",1, freq[0]);
+    // printf("%d -> %d\n",2, freq[1]);
+    // printf("%d -> %d\n",3, freq[2]);
+    // printf("%d -> %d\n",4, freq[3]);
+    // printf("%d -> %d\n",5, freq[4]);
+    // printf("%d -> %d\n",6, freq[5]);
 
     return 0;
 }
