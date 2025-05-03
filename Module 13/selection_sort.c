248324c197be8ef39873629f6905aca17a8f136d
Amitsengupta332 <amitsengupta332@gmail.com>
2025-05-04 02:05:44 +0600

    done selection sort

--- Module 13/selection_sort.c
+++ Module 13/selection_sort.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 int main()
 {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+     
+
     
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            // printf("%d %d\n", a[i], a[j]);
+            // if (a[i]<a[j])
+            // {
+            //  // serial ok
+            // }else
+            if(a[i]> a[j])
+            {
+                // not serial ok
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+            
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+        /* code */
+    }
+    
+   
+
     return 0;
 }
