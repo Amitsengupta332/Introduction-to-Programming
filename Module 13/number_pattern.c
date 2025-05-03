6347520556ddf70fdb7d429cf62591c80f0daee2
Amitsengupta332 <amitsengupta332@gmail.com>
2025-05-04 01:11:11 +0600

    Add .gitignore to skip exe and temp files

--- Module 13/number_pattern.c
+++ Module 13/number_pattern.c
@@ -3,24 +3,24 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    int star = 1;
-    int space = n-1 ;
-    for (int i = 1; i <=n; i++) // for printing line
+    int val = 1;
+    int space = n - 1;
+    for (int i = 1; i <= n; i++) // for printing line
     {
-        for (int j =1; j<=space ; j++)
+        for (int j = 1; j <= space; j++)
         {
             printf(" ");
         }
-        for (int j = 1; j <= star; j++) // for printing star
+        for (int j = 1; j <= val; j++) // for printing value
         {
-            printf("*");
+            printf("%d ", j);
             /* code */
         }
         printf("\n");
-        star ++;
+       val++;
         // star +=2;
         space--;
-        
+
         // printf("kaj");
     }
     return 0;
