ae432f52acf699e7759a1a8a3ef8bc9db1b716d2
Amitsengupta332 <amitsengupta332@gmail.com>
2025-05-03 20:46:54 +0600

    done star print 1

---
+++ Module 13/star_pattern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int n,star=1;
+    scanf("%d", &n);
+    for (int i = 1; i <=n; i++) // for printing line
+    {
+        for (int j = 1; j <=star; j++) // for printing star
+        {
+            printf("*");
+            /* code */
+        }
+        printf("\n");
+        star++;
+        
+        // printf("kaj");
+        /* code */
+    }
+    
+    return 0;
+}
