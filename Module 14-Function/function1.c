1ae292fc41d3e232af43f3eb2149d537c8e10a1f
Amitsengupta332 <amitsengupta332@gmail.com>
2025-05-05 01:01:18 +0600

    user input in function parameter

--- Module 14-Function/function1.c
+++ Module 14-Function/function1.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+// int sub(int a, int b)
+// {
+//     int answer = a - b;
+//     return answer;
+// }
 int sum(int a, int b)
 {
     int answer = a + b;
@@ -7,8 +12,12 @@ int sum(int a, int b)
 }
 int main()
 {
-    int val = sum(10, 5);
-    printf("%d", val);
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int val = sum(a, b);
+    // int val1 = sum(10, 5);
+    // int val2 = sub(10, 5);
+    printf("%d  ", val);
 
     return 0;
 }
