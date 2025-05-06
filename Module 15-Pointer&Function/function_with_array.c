85105f241ba8fa3838bf8d5f716941da285d4c9a
Amitsengupta332 <amitsengupta332@gmail.com>
2025-05-07 01:34:45 +0600

    function array

--- Module 15-Pointer&Function/function_with_array.c
+++ Module 15-Pointer&Function/function_with_array.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+void fun(int a[], int n)
+{
+    // a[1] = 200;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    fun(a,n);
+    // int a[5] = {10,20,30,40,50};
+    // for(int i = 0; i < 5; i++)
+    // {
+    //     printf("%d ", a[i]);
+    // }
     return 0;
 }
