b4afd45053b1bd0b50f5361041c0c3347cf457ca
Amitsengupta332 <amitsengupta332@gmail.com>
2025-05-06 23:54:42 +0600

    why use scanf

---
+++ Module 15-Pointer&Function/scan_function.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// void scanf(int x)
+// {
+//     // code how to take input
+// }
+int main()
+{
+    int x;
+    scanf("%d", &x); // scanf function er maddhome input neya hoyeche.
+    printf("%d\n", x);
+    return 0;
+}
