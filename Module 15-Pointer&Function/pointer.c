9d16d59c14e9bcd77870fc8eef68e38a5720155e
Amitsengupta332 <amitsengupta332@gmail.com>
2025-05-06 21:32:09 +0600

    dereferencing a pointer

--- Module 15-Pointer&Function/pointer.c
+++ Module 15-Pointer&Function/pointer.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int x= 10;
+    int x= 100;
     printf("%d\n", x);
-    printf("%p\n", &x);
+    // printf("%p\n", &x);
 
     int* ptr;
     ptr = &x; // x er address rakhlam. 
-    printf("%p\n", ptr);
-    printf("%p\n", &ptr);
+    *ptr = 200; // dereference kore value change korlam.
+    // printf("%p\n", ptr);
+    // printf("%p\n", &ptr);
+    printf("%d\n", *ptr);
     return 0;
 }
