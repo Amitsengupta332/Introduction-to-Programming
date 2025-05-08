ae10c014ec6d91ffe8761eb1f9be8c57e455f9cc
Amitsengupta332 <amitsengupta332@gmail.com>
2025-05-09 01:06:25 +0600

    assignment 1

--- Module 16- Assignment/a1.c
+++ Module 16- Assignment/a1.c
@@ -5,13 +5,45 @@ int main()
     scanf("%d", &n);
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; i <= 5; j++)
+        for (int j = 0; j < n - i; j++)
         {
-            /* code */
-            printf("#\n");
+            printf(" ");
         }
 
-        /* code */
+        if (i % 2 == 1)
+        {
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                printf("#");
+            }
+        }
+        else
+        {
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                printf("-");
+            }
+        }
+        printf("\n");
+    }
+
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            printf(" ");
+        }
+
+        if (i % 2 == 1) {
+            for (int j = 0; j < 2 * i - 1; j++) {
+                printf("#");
+            }
+        } else {
+            for (int j = 0; j < 2 * i - 1; j++) {
+                printf("-");
+            }
+        }
+        printf("\n");
     }
 
     return 0;
