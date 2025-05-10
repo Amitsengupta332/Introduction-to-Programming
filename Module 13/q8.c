f9c6e946d002c69300c489d23a884e72f77fb9b5
Amitsengupta332 <amitsengupta332@gmail.com>
2025-05-10 13:59:49 +0600

    conceptual session

--- Module 13/q8.c
+++ Module 13/q8.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    for (int i = 1; i <=5; i+=2)
+    for (int i = 3; i >=1; i--)
     {
-        for (int j = 1; j <=i; j++)
+        for (int j = 1;  j<=i; j++)
         {
-            // printf("%d", i );
             printf("*");
             /* code */
         }
-        printf("\n");
+        printf(" ");
         
         /* code */
     }
-    
     
     return 0;
 }
