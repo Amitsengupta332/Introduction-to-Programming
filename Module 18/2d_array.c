410f01f3ec400f8656dbb6c6ba8d21f7b62fbd7f
Amitsengupta332 <amitsengupta332@gmail.com>
2025-05-12 01:51:34 +0600

    done 2d array

--- Module 18/2d_array.c
+++ Module 18/2d_array.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 int main()
 {
-    int a = 5;
-    printf("%d\n", a);
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int a[r][c];
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
