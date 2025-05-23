5c30df8d3380874f957cfa1481d4083a4ce56c71
amitsengupta <amitsengupta332@gmail.com>
2025-05-23 22:57:20 +0600

    nobita

--- Contest/divisible.c
+++ Contest/divisible.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main()
-{
+int main() {
+    char number[101];  
+    scanf("%s", number);
+    
+    int length = strlen(number);
+    int odd_sum = 0, even_sum = 0;
+    
+    for (int i = 0; i < length; i++) {
+        int digit = number[length - 1 - i] - '0';  
+        if ((i + 1) % 2 == 1) {  
+            odd_sum += digit;
+        } else { // Even position
+            even_sum += digit;
+        }
+    }
+    
+    int difference = abs(odd_sum - even_sum);
+    if (difference % 11 == 0) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+    
     return 0;
 }
--- Contest/nobita.c
+++ Contest/nobita.c
@@ -2,5 +2,45 @@
 
 int main()
 {
+    int n,k;
+    scanf("%d %d", &n, &k);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        /* code */
+    }
+    // sort the array
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n-i-1 ; j++)
+        {
+             if (a[j] > a[j + 1]) {
+                // Swap
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+ 
+    }
+
+    int left = 0, right = n-1, shots = 0;
+    while (left < right)
+    {
+        if (a[left] + a[right] <= k)
+        {
+            shots++;
+            left++;
+            right--;
+            
+        }else{
+            right--;
+        }
+        
+        /* code */
+    }
+    
+    printf("%d\n", shots);
     return 0;
 }
