731f61aff2a8d2fc87b9fe3198b3df73be092b01
amitsengupta <amitsengupta332@gmail.com>
2026-04-14 13:48:07 +0600

    upgrade the task

--- Conceptual Session/week two session/modifyproblem1toN.c
+++ Conceptual Session/week two session/modifyproblem1toN.c
@@ -6,15 +6,35 @@ int main()
     scanf("%d", &N);
 
     // 1 to N
-    // for (int i = 1; i <= N; i++)
-    // {
-    //     printf("%d\n", i);
-    // }
-
-    // N to 1
-    for(int i = N; i >=1; i--)
+    for (int i = 1; i <= N; i++)
     {
         printf("%d\n", i);
     }
+
+    // Task1:  N to 1
+    for (int i = N; i >= 1; i--)
+    {
+        printf("%d\n", i);
+    }
+
+    // Task2 : Print Even number
+    for (int i = 0; i <= N; i++)
+    {
+        if (i % 2 == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    // Task3: 1 to N sum
+
+    int sum = 0;
+
+    for (int i = 1; i <= N; i++)
+    {
+        sum += i;
+    }
+    printf("%d\n", sum);
+
     return 0;
 }
