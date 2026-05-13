8cd61b4a581e5f253267ee3aed02ac7f8203990b
amitsengupta <amitsengupta332@gmail.com>
2026-05-14 01:11:26 +0600

    loop practice

---
+++ Loop_Practice/While_Loop/problem2.cpp
@@ -0,0 +1,16 @@
+// Print numbers from 10 down to 1 in reverse order. 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int i = 10;
+  while (i>=1)
+  {
+    cout << i << " " ;
+    i--;
+  }
+  
+    return 0;
+}
