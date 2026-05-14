1200bc7de053b0a0fb70a450f36d148e0bd43e5f
amitsengupta <amitsengupta332@gmail.com>
2026-05-14 22:05:28 +0600

    arry reversed

--- Array/array.c
+++ Array/array.c
@@ -1,9 +1,21 @@
- #include <stdio.h>
- 
- int main()
- {
-    int arr[5];
-    arr[2] = 100;
-    printf("%d", arr[2]);
-    return 0;
- }
+#include <stdio.h>
+
+int main()
+{
+   int n;
+   scanf("%d", &n);
+   int arr[n];
+
+   for (int i = 0; i < n; i++)
+   {
+      scanf("%d", &arr[i]);
+   }
+
+   for (int i = 0; i < n; i++)
+   {
+      printf("%d ", arr[i]);
+   }
+   
+
+   return 0;
+}
